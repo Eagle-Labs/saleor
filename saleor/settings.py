7ec2be1ca93727c179dec1aab0b5b66a26a16edd
hgun0888 <Heshan.Gunarathne@syscolabs.com>
2021-07-17 18:04:16 +0530

    updated allowed client hosts

--- saleor/settings.py
+++ saleor/settings.py
@@ -51,7 +51,7 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
-_DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1"
+_DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1,interflexlabs-saleor-dashboard.herokuapp.com"
 
 ALLOWED_CLIENT_HOSTS = os.environ.get("ALLOWED_CLIENT_HOSTS")
 if not ALLOWED_CLIENT_HOSTS:
